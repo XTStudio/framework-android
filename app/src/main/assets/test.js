7406abfcbdccaed2c50cc1d6a8543b188b0d0c1c
PonyCui <cuis@vip.qq.com>
2018-08-07 18:18:33 +0800

    Feature: add DispatchQueue exports.

--- app/src/main/assets/test.js
+++ app/src/main/assets/test.js
@@ -1,9 +1,10 @@
 var main = new UIView
 main.frame = { x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height }
 
-var filePath = FileManager.temporaryDirectory + '/s/ttt.txt'
-// var toPath = FileManager.temporaryDirectory + '/eee.txt'
-FileManager.defaultManager.createFile(filePath, new Data({ utf8String: 'Hello, World!' }))
-// FileManager.defaultManager.moveItem(filePath, toPath)
-// var e = FileManager.defaultManager.readFile(toPath)
-console.log(FileManager.defaultManager.subpaths(FileManager.temporaryDirectory))
+var data = new Data({ utf8String: "Main Thread String" })
+
+DispatchQueue.global.isolate(function () {
+    DispatchQueue.global.asyncAfter(3.0, function () {
+        console.log("fdhslkajfhl")
+    })
+})
--- library/src/main/java/com/xt/kimi/foundation/DispatchQueue.kt
+++ library/src/main/java/com/xt/kimi/foundation/DispatchQueue.kt
@@ -3,14 +3,20 @@ package com.xt.kimi.foundation
 import android.os.Handler
 import android.os.Looper
 import android.os.SystemClock
+import com.eclipsesource.v8.utils.V8Executor
 import com.xt.endo.EDOCallback
+import com.xt.endo.EDOExporter
+import com.xt.endo.EDOObjectTransfer
+import com.xt.jscore.JSContext
 import com.xt.kimi.KIMIPackage
 
 class DispatchQueue(val identifier: String) {
 
     private val handler: Handler
+    private var operationContext: JSContext? = null
 
     init {
+        assert(identifier == "main" && Looper.myLooper() == Looper.getMainLooper())
         if (Looper.myLooper() == null) {
             Looper.prepare()
         }
@@ -18,20 +24,61 @@ class DispatchQueue(val identifier: String) {
     }
 
     fun async(asyncBlock: EDOCallback) {
+        if (this.handler.looper != Looper.myLooper()) {
+            return
+        }
         this.handler.post {
             asyncBlock.invoke()
         }
     }
 
     fun asyncAfter(delayInSeconds: Double, asyncBlock: EDOCallback) {
+        if (this.handler.looper != Looper.myLooper()) {
+            return
+        }
         this.handler.postAtTime({
             asyncBlock.invoke()
         }, SystemClock.uptimeMillis() + (delayInSeconds * 1000).toLong())
     }
 
+    fun _isolate(script: String, arguments: List<Any>) {
+        this.handler.post {
+            if (this.operationContext == null) {
+                val context = JSContext()
+                EDOExporter.sharedExporter.exportWithContext(context)
+                try {
+                    val logClazz = Class.forName("com.xt.uulog.UULog\$Companion")
+                    val method = logClazz.getDeclaredMethod("attachTo", JSContext::class.java)
+                    val companionObject = Class.forName("com.xt.uulog.UULog").getDeclaredField("Companion").get(Class.forName("com.xt.uulog.UULog"))
+                    method.invoke(companionObject, context)
+                } catch (e: Exception) {
+                    print(true)
+                }
+                this.operationContext = context
+            }
+            this.operationContext?.let { context ->
+                context.evaluateScript("var __isolate_exec_func = $script")
+                context["__isolate_exec_func"]?.callWithArguments(arguments.map {
+                    return@map EDOObjectTransfer.convertToJSValueWithJavaValue(it, context.runtime)
+                })
+            }
+        }
+    }
+
     companion object {
 
         @JvmStatic val main = DispatchQueue("main")
+
+        @JvmStatic var global: DispatchQueue? = null
+
+        fun createGlobalQueue() {
+            if (this.global == null) {
+                Thread {
+                    this.global = DispatchQueue("global")
+                    Looper.loop()
+                }.start()
+            }
+        }
 
     }
 
@@ -41,12 +88,16 @@ fun KIMIPackage.installDispatchQueue() {
     exporter.exportClass(DispatchQueue::class.java, "DispatchQueue")
     exporter.exportMethodToJavaScript(DispatchQueue::class.java, "async")
     exporter.exportMethodToJavaScript(DispatchQueue::class.java, "asyncAfter")
-    exporter.exportInitializer(DispatchQueue::class.java, {
+    exporter.exportMethodToJavaScript(DispatchQueue::class.java, "_isolate")
+    exporter.exportInitializer(DispatchQueue::class.java) {
         val identifier = it.firstOrNull() as? String ?: return@exportInitializer DispatchQueue.main
         if (identifier == "main") {
             return@exportInitializer DispatchQueue.main
         }
-        return@exportInitializer DispatchQueue.main
-    })
+        return@exportInitializer DispatchQueue(identifier)
+    }
     exporter.exportStaticProperty(DispatchQueue::class.java, "main", true)
+    exporter.exportStaticProperty(DispatchQueue::class.java, "global", true)
+    exporter.exportScript(DispatchQueue::class.java, "Initializer.prototype.isolate = function(){ var args = []; for(var i=1;i<arguments.length;i++){ args.push(arguments[i]); } this._isolate(arguments[0].toString(), args); }", true)
+    DispatchQueue.createGlobalQueue()
 }
