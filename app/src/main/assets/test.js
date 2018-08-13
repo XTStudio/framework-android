f412dd824f2095eb0a799e50866f17ff3966355f
PonyCui <cuis@vip.qq.com>
2018-08-13 21:12:50 +0800

    Feature: add loadImageWithURLString to UIImageView.

--- app/src/main/assets/test.js
+++ app/src/main/assets/test.js
@@ -3,12 +3,13 @@ class FooViewController extends UIViewController {
     viewDidLoad() {
         super.viewDidLoad()
         this.title = "Test"
-        this.view.addGestureRecognizer(new UITapGestureRecognizer().on("touch", function(){
-                var sub = new UINavigationBarViewController
-                sub.navigationBar.backgroundColor = UIColor.yellow
-                sub.view.backgroundColor = UIColor.green
-                this.navigationController.pushViewController(sub)
-            }.bind(this)))
+        var imageView = new UIImageView
+        imageView.frame = {x: 44, y: 44, width: 200, height: 88}
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+//        imageView.layer.cornerRadius = 44
+        imageView.layer.masksToBounds = true
+        imageView.loadImageWithURLString("http://img.hb.aicdn.com/625d4850d10e332a08c5655b9c6e30d9143138cfee08b-g7Ar2k_sq320")
+        this.view.addSubview(imageView)
     }
 
 }
@@ -16,8 +17,3 @@ class FooViewController extends UIViewController {
 
 var main = new UINavigationController(new FooViewController)
 
-
-
-//sub.view.addGestureRecognizer(new UITapGestureRecognizer().on("touch", function(){
-//                                                              sub.navigationController.pushViewController(new UIViewController())
-//                                                              }))
--- library/src/main/java/com/xt/kimi/uikit/UIImageView.kt
+++ library/src/main/java/com/xt/kimi/uikit/UIImageView.kt
@@ -1,5 +1,10 @@
 package com.xt.kimi.uikit
 
+import android.graphics.Color
+import android.view.ViewGroup
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.drawable.ScalingUtils
+import com.facebook.drawee.view.SimpleDraweeView
 import com.xt.endo.CGSize
 import com.xt.kimi.KIMIPackage
 
@@ -7,9 +12,31 @@ class UIImageView: UIView() {
 
     var image: UIImage? = null
         set(value) {
+            this.simpleDraweeView?.setImageURI(null as? String)
             field = value
             this.layer.contents = value
             this.setNeedsDisplay()
+        }
+
+    private var simpleDraweeView: SimpleDraweeView? = null
+
+    override var contentMode: UIViewContentMode
+        get() = super.contentMode
+        set(value) {
+            super.contentMode = value
+            this.simpleDraweeView?.let {
+                when (value) {
+                    UIViewContentMode.scaleAspectFit -> {
+                        it.hierarchy.actualImageScaleType = ScalingUtils.ScaleType.FIT_CENTER
+                    }
+                    UIViewContentMode.scaleToFill -> {
+                        it.hierarchy.actualImageScaleType = ScalingUtils.ScaleType.FIT_XY
+                    }
+                    UIViewContentMode.scaleAspectFill -> {
+                        it.hierarchy.actualImageScaleType = ScalingUtils.ScaleType.CENTER_CROP
+                    }
+                }
+            }
         }
 
     override fun intrinsicContentSize(): CGSize? {
@@ -19,9 +46,30 @@ class UIImageView: UIView() {
         return super.intrinsicContentSize()
     }
 
+    fun loadImageWithURLString(URLString: String, placeholder: UIImage?) {
+        if (simpleDraweeView == null) {
+            simpleDraweeView = SimpleDraweeView(this.context)
+            this.contentMode = this.contentMode
+            addView(simpleDraweeView, ViewGroup.LayoutParams((this.bounds.width * com.xt.kimi.uikit.scale).toInt(), (this.bounds.height * com.xt.kimi.uikit.scale).toInt()))
+        }
+        this.image = null
+        this.simpleDraweeView?.setImageURI(URLString)
+        this.simpleDraweeView?.setBackgroundColor(Color.RED)
+    }
+
+    override fun layoutSubviews() {
+        super.layoutSubviews()
+        this.simpleDraweeView?.let {
+            this.removeView(it)
+            this.addView(it, ViewGroup.LayoutParams((this.bounds.width * com.xt.kimi.uikit.scale).toInt(), (this.bounds.height * com.xt.kimi.uikit.scale).toInt()))
+        }
+    }
+
 }
 
 fun KIMIPackage.installUIImageView() {
+    Fresco.initialize(exporter.applicationContext)
     exporter.exportClass(UIImageView::class.java, "UIImageView", "UIView")
     exporter.exportProperty(UIImageView::class.java, "image")
+    exporter.exportMethodToJavaScript(UIImageView::class.java, "loadImageWithURLString")
 }
